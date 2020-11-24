4bb7d6d8822967c6d9d71b8e8c3c3914ac7528d8
Joshua Iceton <Iceto.J01@elliotthudsoncollege.ac.uk>
2020-11-24 14:45:40 +0000

    session 7

---
+++ Assets/Scripts/heroAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroAnimation : MonoBehaviour {
+	Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			myAnimator.SetBool ("walk", true);
+		}
+		else {
+			myAnimator.SetBool ("walk", false);
+	}
+}
+}
